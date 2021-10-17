a17dc486230bbe1851b34bfb012f86538b4ee9b3
yubin <yb_970626@naver.com>
2021-10-17 17:40:52 +0900

    initial commit test

---
+++ app/config.py
@@ -0,0 +1,26 @@
+
+# DB 관련
+DB = {
+    'host': '127.0.0.1', 
+    'port': 3306,
+    'user_id': 'root',
+    'user_pw': 'root',
+    'database': 'we',
+    'charset': 'utf8'
+}
+
+# 이미지 경로 관련
+IMAGH_PATH = {
+    
+}
+
+# 검색 화면 관련
+SEARCH = {
+    'SEARCH_RESULT_COUNT': 5,
+    'SEARCH_PAGE_COUNT': 5
+}
+
+# 날짜 관련
+DATE = {
+    "WEEKDAY_LIST": ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+}
